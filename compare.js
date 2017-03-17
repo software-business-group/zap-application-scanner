63790d0cdd288b0185fe02a4c317de3c4c42e56e
Sebastian Krysiak <sebastian.krysiak@sbg.com.pl>
2017-03-17 11:18:34 +0100

    compare url fixes

--- compare.js
+++ compare.js
@@ -8,7 +8,8 @@ var results_one, results_two, diff,
     file_one = __dirname + '/tmp/results-one.json',
     file_two = __dirname + '/tmp/results-two.json',
     out_file = __dirname + '/results/results-from-' + new Date()+ '.json',
-    parameters = config.get(application);
+    parameters = config.get(application),
+    possible_tokens = ['csrf', 'token', 'csrf_token'];
 
 results_one = jsonfile.readFileSync(file_one);
 results_two = jsonfile.readFileSync(file_two);
@@ -28,10 +29,10 @@ function getDiff(obj1, obj2){
 
        if(parameters.ignored_codes.indexOf(elem.statusCode) == -1){
 
-           var elem_clean_url = elem.url.replace(new RegExp("[0-9]", "g"), "X");
+           var elem_clean_url = cleanUrl(elem.url);
 
            obj2.forEach(function(elem2){
-               var elem2_clean_url = elem2.url.replace(new RegExp("[0-9]", "g"), "X");
+               var elem2_clean_url = cleanUrl(elem2.url);
 
                if( elem_clean_url == elem2_clean_url
                    && elem.method == elem2.method
@@ -46,4 +47,23 @@ function getDiff(obj1, obj2){
        }
     });
     return diff;
+}
+
+function cleanUrl(url){
+    url = decodeURIComponent(url);
+    url = url.replace(/\d+/g, "X");
+    possible_tokens.forEach(function(token){
+        url = replaceUrlParam(url, token, "X");
+    });
+    return url;
+}
+
+function replaceUrlParam(url, paramName, paramValue){
+    if(paramValue == null)
+        paramValue = '';
+    var pattern = new RegExp('\\b('+paramName+'=).*?(&|$)')
+    if(url.search(pattern)>=0){
+        return url.replace(pattern,'$1' + paramValue + '$2');
+    }
+    return url + (url.indexOf('?')>0 ? '&' : '?') + paramName + '=' + paramValue
 }
